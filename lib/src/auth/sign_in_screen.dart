91649500164cbf3c919ec4e844ad49b263edff73
jefferson-norberto2 <97047815+jefferson-norberto2@users.noreply.github.com>
2022-11-20 17:12:33 -0300

    Update sign_in_screen.dart

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -15,50 +15,92 @@ class SignInScreen extends StatelessWidget {
               color: Colors.green,
             ),
           ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(40),
-                ),
-                color: Colors.white,
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(40),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // Email
-                  const CustomTextField(icon: Icons.email, label: 'Email'),
-                  // Password
-                  const CustomTextField(
-                      icon: Icons.lock, label: 'Password', isSecrect: true),
-                  // Sign In Button
-                  SizedBox(
+              color: Colors.white,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Email
+                const CustomTextField(icon: Icons.email, label: 'Email'),
+
+                // Password
+                const CustomTextField(
+                    icon: Icons.lock, label: 'Password', isSecrect: true),
+
+                // Sign In Button
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+
+                // Forgot Password Button
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.redAccent),
+                      )),
+                ),
+
+                // Divider
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey[400],
+                        thickness: 1,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text('OR'),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey[400],
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Create Account Button
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: SizedBox(
                     height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Colors.green, width: 2),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                      onPressed: () {},
-                      child: const Text(
-                        'Sign In',
-                        style: TextStyle(fontSize: 18),
-                      ),
+                      child: const Text('Create Account'),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.redAccent),
-                        )),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ],
