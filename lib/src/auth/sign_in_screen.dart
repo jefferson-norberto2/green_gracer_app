b2c49edb9b159131d631b90a78a918365432c17b
jefferson-norberto2 <97047815+jefferson-norberto2@users.noreply.github.com>
2022-11-20 16:42:32 -0300

    Feat - Button sign and forgot password

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -25,12 +25,38 @@ class SignInScreen extends StatelessWidget {
                 color: Colors.white,
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   // Email
-                  CustomTextField(icon: Icons.email, label: 'Email'),
+                  const CustomTextField(icon: Icons.email, label: 'Email'),
                   // Password
-                  CustomTextField(
+                  const CustomTextField(
                       icon: Icons.lock, label: 'Password', isSecrect: true),
+                  // Sign In Button
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.redAccent),
+                        )),
+                  ),
                 ],
               ),
             ),
