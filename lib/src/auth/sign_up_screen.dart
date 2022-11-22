e1d4041052d4bb89e00cc43d17828b3d479389fb
jefferson-norberto2 <97047815+jefferson-norberto2@users.noreply.github.com>
2022-11-22 13:34:33 -0300

    Feat - Sign Up Screen maked

---
+++ lib/src/auth/sign_up_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:green_gracer_app/src/auth/components/custom_text_field.dart';
+import 'package:green_gracer_app/src/config/custom_colors.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              const Expanded(
+                child: Center(
+                  child: Text(
+                    'Cadastro',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                    ),
+                  ),
+                ),
+              ),
+
+              // Form fields
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(40),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const CustomTextField(
+                      icon: Icons.person,
+                      label: 'Nome',
+                    ),
+                    const CustomTextField(
+                      icon: Icons.email,
+                      label: 'Email',
+                    ),
+                    const CustomTextField(
+                      icon: Icons.lock,
+                      label: 'Senha',
+                      isSecrect: true,
+                    ),
+                    const CustomTextField(
+                      icon: Icons.phone,
+                      label: 'Celular',
+                    ),
+                    const CustomTextField(
+                      icon: Icons.file_copy,
+                      label: 'CPF',
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Create User',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
